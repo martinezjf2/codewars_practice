f42cf57f3ec1dfb99f15bc7dd023aff5c75bc580
Jeffrey Martinez <martinezjf2@gmail.com>
2023-04-14 20:44:55 -0400

    Done with if/else syntax debugging

---
+++ if_else_syntax_debugging.py
@@ -0,0 +1,21 @@
+
+# Starting Code:
+
+def check_alive(health):
+    if():
+        health < 0
+        return false
+    else:
+        return true
+    
+# Final Code:
+
+def check_alive(health):
+    if (health <= 0):
+        return False
+    else:
+        return True
+
+
+
+# Link to Codewars: https://www.codewars.com/kata/grasshopper-if-slash-else-syntax-debug
