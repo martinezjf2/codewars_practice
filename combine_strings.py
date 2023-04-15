18a573d252801da25693b14200158c7f1f3a28ca
Jeffrey Martinez <martinezjf2@gmail.com>
2023-04-14 21:29:42 -0400

    done with combine_strings

--- combine_strings.py
+++ combine_strings.py
@@ -1,10 +1,11 @@
 # Starter Code
 
-
+# Create the combine_names function here
 
 # Final Code
 
-
+def combine_names(first, last):
+    return f"{first} {last}"
 
 
 # Link to codewars: https://www.codewars.com/kata/grasshopper-combine-strings
