cc57ba144ee3bf633051eeb96df88fc82aee2c9a
Jeffrey Martinez <martinezjf2@gmail.com>
2023-04-14 21:00:45 -0400

    Done with summation

---
+++ summation.py
@@ -0,0 +1,16 @@
+# Starter Code
+
+def summation(num):
+    pass 
+
+
+# Final Code
+
+def summation(num):
+    a = range(0, num+1)
+    b = 0
+    for x in a:
+        b = b + x
+    return b
+
+# Link to codewars: https://www.codewars.com/kata/grasshopper-summation
