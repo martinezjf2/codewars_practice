9e19bbe3c9c23f66b2ebceb3422872a5fee941cd
Jeffrey Martinez <martinezjf2@gmail.com>
2023-04-14 21:25:46 -0400

    Done with personalized message

---
+++ personalized_message.py
@@ -0,0 +1,16 @@
+# Starter code
+def greet(name, owner):
+    pass
+
+
+
+# Final Code
+
+def greet(name, owner):
+    if name == owner:
+        return 'Hello boss'
+    else:
+        return 'Hello guest'
+
+
+# Link to codewars: https://www.codewars.com/kata/grasshopper-personalized-message
