750105def60f99058cafd8b1a5fb9b0609462d86
Jeffrey Martinez <martinezjf2@gmail.com>
2023-04-14 21:36:08 -0400

    done with array_mean

---
+++ array_mean.py
@@ -0,0 +1,16 @@
+# Starter Code
+
+def find_average(nums):
+    pass
+
+
+# Final Code
+
+def find_average(nums):
+    if nums:
+        return sum(nums) / len(nums) 
+    else:
+        return 0
+
+
+# Link to Codewars: https://www.codewars.com/kata/grasshopper-array-mean
