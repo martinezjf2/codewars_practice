5fdba9f21e5cf572917a3cd39ebbe82be8702933
Jeffrey Martinez <martinezjf2@gmail.com>
2023-04-14 21:58:27 -0400

    Done with is_the_string_uppercase

---
+++ is_the_string_upperase.py
@@ -0,0 +1,16 @@
+# Starter code: 
+
+def is_uppercase(inp):
+    return False
+
+
+
+# Final Code:
+
+def is_uppercase(inp):
+    if inp == inp.upper():
+        return True
+    else:
+        return False
+
+# Link to codewars: https://www.codewars.com/kata/is-the-string-uppercase
