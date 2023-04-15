d883750b3466e0cf2194da34fd8d0c46e8fe502e
Jeffrey Martinez <martinezjf2@gmail.com>
2023-04-14 21:18:04 -0400

    Done with sum of multiples

---
+++ sum_of_multiples.py
@@ -0,0 +1,30 @@
+# Starter Code
+
+def sum_mul(n, m):
+    pass
+
+# Final Code
+
+def sum_mul(n, m):
+    sumu = 0
+    if n > 0 and m > 0:
+        for i in range(n, m):
+            if (i % n == 0):
+                sumu += i
+    else:
+        return 'INVALID'
+    return sumu
+
+# or
+
+def sum_mul(n, m):
+    L=[]
+    if n<=0 or m<=0:
+        return 'INVALID'
+    else:
+        for i in range(0,m):
+            if i*n<m:
+                L.append(i*n)
+        return sum(L)
+
+# Link to CodeWars: https://www.codewars.com/kata/sum-of-multiples
