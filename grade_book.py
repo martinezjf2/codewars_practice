0f029fddbb46009e94d0ee8dc50d011dfddfe65c
Jeffrey Martinez <martinezjf2@gmail.com>
2023-04-14 20:40:00 -0400

    Done grade_book

---
+++ grade_book.py
@@ -0,0 +1,16 @@
+def get_grade(s1, s2, s3):
+    average = (s1 + s2 + s3) / 3
+
+    if average >= 90 and average <= 100:
+        return 'A'
+    elif average >= 80 and average < 90:
+        return 'B'
+    elif average >= 70 and average < 80:
+        return 'C'
+    elif average >= 60 and average < 70:
+        return 'D'
+    elif average < 60:
+        return 'F'
+    
+    
+# Link to Codegrade: https://www.codewars.com/kata/grasshopper-grade-book
