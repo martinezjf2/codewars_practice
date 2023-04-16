89f4a62d83426514a2be99bafbbc5a4c091e2357
Jeffrey Martinez <martinezjf2@gmail.com>
2023-04-15 21:30:40 -0400

    done with needle in a haystack

---
+++ a_needle_in_a_haystack.py
@@ -0,0 +1,18 @@
+# Starter
+
+def find_needle(haystack):
+    pass
+
+
+# Final
+
+def find_needle(haystack):
+    needle = haystack.index('needle')
+    if needle:
+        return f"found the needle at position {needle}"
+    else:
+        return f"found the needle at position 0"
+    # your code here
+
+
+# Link to Codewars: https://www.codewars.com/kata/a-needle-in-the-haystack
