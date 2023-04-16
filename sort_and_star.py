04939fb61c3aa0609e5db3b9c0175a8c18fff80a
Jeffrey Martinez <martinezjf2@gmail.com>
2023-04-15 21:45:27 -0400

    done with sort and star

---
+++ sort_and_star.py
@@ -0,0 +1,16 @@
+# Starter
+
+def two_sort():
+    pass
+
+# Final
+
+def two_sort(array):
+    array.sort()
+    first = array[0]
+    newList = [a for a in first]
+    end = '***'.join(newList)
+    return end
+
+
+# Link to codewars: https://www.codewars.com/kata/sort-and-star
