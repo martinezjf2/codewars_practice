13391d4a6d32f68f2241e6d198809907253beece
Jeffrey Martinez <martinezjf2@gmail.com>
2023-04-15 21:22:19 -0400

    done with sum mixed array

---
+++ sum_mixed_array.py
@@ -0,0 +1,16 @@
+# Starter
+
+def sum_mix(arr):
+    pass
+
+
+# Final:
+
+def sum_mix(arr):
+    total = 0
+    for x in arr:
+        number = int(x)
+        total += number
+    return total
+
+# Link to codewars: https://www.codewars.com/kata/sum-mixed-array
